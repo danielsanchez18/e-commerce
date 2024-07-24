477456f2e3af7476aacb4e538ff33ac4795e8f47
danielsanchez18 <dsanchez151r@gmail.com>
2024-07-24 02:02:12 -0500

    initial project

---
+++ src/test/java/com/dsac/ecommer_backend/EcommerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsac.ecommer_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
